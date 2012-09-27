de3790b9ce8cbd8a4d7a42038d9b35a294b6c678
Agustín Leñero <agush@roca9.com>
2012-09-27 15:04:58 -0500

    Field and Field Type factories

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :field do
+    name "Real Canchas"
+    address "Direccion x"
+    city "Ciudad x"
+    state "Estado x"
+    country "Pais x"
+    street "Calle x"
+  end
+  factory :field_type do
+    name "Fut 7"
+  end
+end
