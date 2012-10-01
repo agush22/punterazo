b089089eadbb37cffdd2884024440d0ccbc35cb3
Agustín Leñero <agush@roca9.com>
2012-10-01 15:19:43 -0500

    Sign in acceptance test

---
+++ spec/requests/sign_ins_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "SignIns" do
+  describe "User Sign In" do
+    it "should sign in a user" do
+      user = FactoryGirl.create(:user)
+      visit new_user_session_path
+      fill_in "Email", :with => user.email
+      fill_in "Password", :with => user.password
+      click_on "Sign in"
+    end
+  end
+  describe "Admin Sign In" do
+    it "should sign in a admin" do
+      user = FactoryGirl.create(:user)
+      visit new_user_session_path
+      fill_in "Email", :with => user.email
+      fill_in "Password", :with => user.password
+      click_on "Sign in"
+      page.should have_content(user.email)
+      page.should have_content('Listing fields')
+    end
+  end
+end
