c9d99cf67b37d2505711f8a6b8649de5c8ab441f
Agustín Leñero <agush@roca9.com>
2012-10-04 20:45:36 -0500

    Add rescue from cancan, redirect to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
   private
   def require_login
     unless current_user
