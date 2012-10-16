5b306b66f10e248dde7cf448b5ea5c6c068f860c
Agustín Leñero <agush@roca9.com>
2012-10-16 15:44:20 -0500

    Add test as a user I can Edit a field I own

--- spec/requests/user_fields_spec.rb
+++ spec/requests/user_fields_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe "Fields" do
   before (:each) do
-    @field = FactoryGirl.create(:field)
     @user = FactoryGirl.create(:user)
+    @field = FactoryGirl.create(:field, :user => @user)
     visit new_user_session_path
     fill_in 'Email', :with => @user.email
     fill_in 'Password', :with => @user.password
@@ -17,6 +17,14 @@ describe "Fields" do
       click_on 'Create Field'
       visit fields_user_path(@user)
       page.should have_content("Field X")
+    end
+  end
+  describe "Edit Field" do
+    it "Edit a field the user owns" do
+      visit fields_user_path(@user)
+      click_on 'Edit'
+      fill_in 'Name', :with => "Edited Field"
+      fill_in 'Link', :with => "editedfield.com"
     end
   end
 end
