e30dd67427b69383bf736d85ed07dada4cdc3bf9
Agustín Leñero <agush@roca9.com>
2013-01-03 16:52:05 -0600

    Move integration tests from spec/requests to spec/features (read: http://alindeman.github.com/2012/11/11/rspec-rails-and-capybara-2.0-what-you-need-to-know.html)

---
+++ spec/features/field_types_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "FieldTypes" do
+  describe "GET /field_types" do
+    it "works! (now write some real specs)" do
+      visit field_types_path
+    end
+  end
+end
